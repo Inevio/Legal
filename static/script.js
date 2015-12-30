28f1139a964589bb1b9963d6710f8e4ffb2eff7b
Jorge Fernandez Manteca <jorge92f@gmail.com>
2015-12-30 16:38:29 +0100

    Second version (no js)

--- static/script.js
+++ static/script.js
@@ -6,9 +6,10 @@ var BROWSER_IE = 1;
 var BROWSER_WEBKIT = 2;
 var BROWSER_TYPE = /webkit/i.test(navigator.userAgent) ? BROWSER_WEBKIT : (/trident/i.test(navigator.userAgent) ? BROWSER_IE : BROWSER_FIREFOX);
 var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var app = $(this);
+var editState = false;
 
 // DOM VARS
+var app = $(this);
 //Text
 var appTitle             = $('.app-title');
 var editExpButtonText    = $('.edit-exp-button span');
@@ -19,7 +20,25 @@ var expDescTitle         = $('.exp-desc .title');
 var expClientTitle       = $('.exp-client .title');
 var expAsignsTitle       = $('.exp-asigns .title');
 var expPeopleTitle       = $('.exp-people .title');
+var more                 = $('.more');
+var titleText            = $('.edit-mode .title, .exp-status .title');
+var internText           = $('.intern');
+var externText           = $('.extern');
+var saveText             = $('.save-exp-button span');
+var cancelText           = $('.cancel-exp-button span');
+var deleteText           = $('.delete-exp-button span');
 
+//Events
+var editExpButton        = $('.edit-exp-button');
+var saveExpButton        = $('.save-exp-button');
+var cancelExpButton      = $('.cancel-exp-button');
+var deleteExpButton      = $('.delete-exp-button');
+var expStatusButton      = $('.exp-status .ui-select-dropdown article');
+
+//Others
+var editPopup            = $('.edit-mode-popup');
+var uiWindowContent      = $('.ui-window-content');
+var expStatusIcon        = $('.exp-status .ui-select-input > i');
 
 // COLOR PALETTE
 var colorPalette = [
@@ -34,6 +53,19 @@ var colorPalette = [
   {name: 'grey' , light: '#97a1a9', text:'#353b43' , border:'#384a59'},
   {name: 'yellow' , light: '#fbe27d', text:'#84740b' , border:'#ffb400'},
 ];
+
+// EVENTS
+editExpButton.on('click', function(){
+  editState ? editMode(false) : editMode(true);
+});
+
+cancelExpButton.on('click', function(){
+  editMode(false);
+});
+
+expStatusButton.on('click', function(){
+  $(this).hasClass('open') ? expStatusIcon.css('background-position', '-109px 0') : expStatusIcon.css('background-position', '-123px 0');
+});
 
 // APP functionality
 var initContacts = function(){
@@ -50,6 +82,35 @@ var setInitialTexts = function(){
   expClientTitle.text(lang.client);
   expAsignsTitle.text(lang.asigns);
   expPeopleTitle.text(lang.people);
+  more.text(lang.more);
+  titleText.text(lang.title);
+  internText.text(lang.internId);
+  externText.text(lang.externId);
+  saveText.text(lang.save);
+  cancelText.text(lang.cancel);
+  deleteText.text(lang.delete);
+}
+
+var editMode = function(mode){
+  if(mode){
+    $('.look-mode').hide();
+    $('.edit-mode').show();
+    drawPopup();
+  }else{
+    $('.edit-mode').hide();
+    $('.look-mode').show();
+    undrawPopup();
+  }
+}
+
+var drawPopup = function(){
+  editPopup.addClass('active');
+  uiWindowContent.css('height', 'calc(100% - 89px)');
+}
+
+var undrawPopup = function(){
+  editPopup.removeClass('active');
+  uiWindowContent.css('height', 'calc(100% - 41px)');
 }
 
 // Program run
